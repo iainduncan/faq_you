3e4430194ae2c3b03b40b8e1bbea052002dbef55
Iain Duncan <iain.duncan@unboxedconsulting.com>
2010-11-16 12:23:03 +0200

    Our first commit , setup of question and answer scaffolds

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FaqYou::Application.config.secret_token = 'e27903dbdafb74be657777ab4b7567163b04adaebafd534e7a386e6330728e97e4e41e0c00cc22457922f0e6bc6081b678602727ed0ff85bccf356ed90cab23c'
---
+++ db/seeds.rb
@@ -0,0 +1,9 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
+#   Mayor.create(:name => 'Daley', :city => cities.first)
+
+Question.create(:question =>'Who is Edmore', :user_id =>'1', :posted_at =>'2010-11-16')
